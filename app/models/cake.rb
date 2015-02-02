dde330639a7022963533b065a39188b5fea0129e
aberracja <barbaraklosowska@gmail.com>
2015-02-02 11:58:19 +0100

    cake validations

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -1,2 +1,6 @@
 class Cake < ActiveRecord::Base
+	validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+	validates :cake_type, presence: true
+	validates :description, presence: true, length: { minimum: 100, maximum: 5000 }
+	
 end
