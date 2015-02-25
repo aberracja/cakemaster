cd27e1e1bf4af418f6284cc02b7f9231e80eaa5d
aberracja <barbaraklosowska@gmail.com>
2015-02-26 00:46:28 +0100

    mailing from sendgrid/heroku second try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
-    port: "587",
+    port: 25,
     domain: "heroku.com",
     authentication: "plain",
     enable_starttls_auto: true,
